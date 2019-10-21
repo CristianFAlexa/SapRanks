5108acf49132fc164f8551b9d3fa68bf7e522e14
I508587 <cristian.alexa@sap.com>
2019-10-21 13:43:29 +0300

    Updated user model to have profile picture, name, disputeWin, disputeLoss and xp.

--- lib/model/UserModel.dart
+++ lib/model/UserModel.dart
@@ -1,18 +1,35 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class UserModel {
   String _uid;
   String _email;
   String _rank;
   String _role;
+  String _profilePicture;
+  String _name;
+  int _disputeWin;
+  int _disputeLoss;
+  int _xp;
 
-  UserModel(this._uid, this._email, this._rank, this._role);
+  UserModel(
+      this._uid,
+      this._email,
+      this._rank,
+      this._role,
+      this._profilePicture,
+      this._name,
+      this._disputeWin,
+      this._disputeLoss,
+      this._xp);
 
   Map<String, dynamic> toJson() => {
-        'name': this._email,
+        'email': this._email,
         'uid': this._uid,
         'rank': this._rank,
         'role': this._role,
+        'profile_picture': this._profilePicture,
+        'name': this._name,
+        'dispute_win': this._disputeWin,
+        'dispute_loss': this._disputeLoss,
+        'xp': this._xp
       };
 
   UserModel.map(dynamic obj) {
@@ -20,7 +37,16 @@ class UserModel {
     this._rank = obj['rank'];
     this._email = obj['email'];
     this._uid = obj['uid'];
+    this._profilePicture = obj['profile_picture'];
+    this._name = obj['name'];
+    this._disputeWin = obj['dispute_win'];
+    this._disputeLoss = obj['dispute_loss'];
+    this._xp = obj['xp'];
   }
+
+  String get name => _name;
+
+  String get profilePicture => _profilePicture;
 
   String get role => _role;
 
@@ -30,19 +56,35 @@ class UserModel {
 
   String get uid => _uid;
 
+  int get disputeLoss => _disputeLoss;
+
+  int get disputeWin => _disputeWin;
+
+  int get xp => _xp;
+
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
     map['role'] = this._role;
     map['rank'] = this._rank;
     map['email'] = this._email;
     map['uid'] = this._uid;
+    map['profile_picture'] = this._profilePicture;
+    map['name'] = this._name;
+    map['dispute_win'] = this._disputeWin;
+    map['dispute_loss'] = this._disputeLoss;
+    map['xp'] = this._xp;
     return map;
   }
 
   UserModel.fromMap(Map<String, dynamic> map) {
     this._role = map['role'];
     this._rank = map['rank'];
-    this._email = map['name'];  // todo : add attribute name and email as separate, modify in login too
+    this._email = map['email'];
     this._uid = map['uid'];
+    this._name = map['name'];
+    this._profilePicture = map['profile_picture'];
+    this._disputeWin = map['dispute_win'];
+    this._disputeLoss = map['dispute_loss'];
+    this._xp = map['xp'];
   }
 }
