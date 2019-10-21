dc093c926512f0475e6f699ca775c58f5a4c92e9
I508587 <cristian.alexa@sap.com>
2019-10-21 13:48:04 +0300

    Small refactoring, added background pic, gradient and changed styling, updated signUp method for persisting a new user.

--- lib/setup/register.dart
+++ lib/setup/register.dart
@@ -3,6 +3,7 @@ import 'package:bored/setup/login.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 
 final db = Firestore.instance;
@@ -16,71 +17,142 @@ class Register extends StatefulWidget {
   _RegisterState createState() => _RegisterState();
 }
 
-// todo: check pass does not work as expected.
-
 class _RegisterState extends State<Register> {
   String _email, _password;
-  String _checkPassword;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: GradientAppBar(
-        gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              // Color.fromRGBO(255, 90, 0, 1),
-              Colors.deepOrange,
-              Color.fromRGBO(255, 173, 52, 1),
-            ]),
-        //title: Text(widget.title),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              // ignore: missing_return
-              validator: (input) {
-                if (input.isEmpty) {
-                  return 'Email required!';
-                }
-              },
-              onSaved: (input) => _email = input,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextFormField(
-              // ignore: missing_return
-              validator: (input) {
-                _checkPassword = input;
-                if (input.length < 8) {
-                  return 'Password must have at least 8 characters!';
-                }
-              },
-              onSaved: (input) => _password = input,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            /*TextFormField(
-              // ignore: missing_return
-              validator: (input) {
-                if (input == _checkPassword) {
-                  return 'Passwords must match!';
-                }
-              },
-              onSaved: (input) => _password = input,
-              decoration: InputDecoration(labelText: 'Reenter password'),
-              obscureText: true,
-            ),*/
-            RaisedButton(
-              onPressed: signUp,
-              child: Text('Register now'),
-            )
-          ],
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "assets/images/register2.jpg",
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
         ),
-      ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: GradientAppBar(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.black87, Colors.black38, Colors.black12]),
+            title: Text(widget.title),
+          ),
+          body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.black12,
+                      Colors.black12,
+                      Colors.black12,
+                      Colors.black26,
+                      Colors.black38,
+                    ]
+                )
+            ),
+            child: Center(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(top: 32),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.2,
+                            height: 50,
+                            padding:
+                            EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(5)),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(color: Colors.black, blurRadius: 2)
+                                ]),
+                            child: TextFormField(
+                              // ignore: missing_return
+                              validator: (input) {
+                                if (input.isEmpty) {
+                                  return 'Email required!';
+                                }
+                              },
+                              onSaved: (input) => _email = input,
+                              decoration: InputDecoration(
+                                  icon: Icon(Icons.mail), hintText: 'Email'),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(top: 32),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  width: MediaQuery.of(context).size.width / 1.2,
+                                  height: 50,
+                                  padding:
+                                  EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                                      color: Colors.white,
+                                      boxShadow: [
+                                        BoxShadow(color: Colors.black, blurRadius: 2)
+                                      ]),
+                                  child:  TextFormField(
+                                    // ignore: missing_return
+                                    validator: (input) {
+                                      if (input.length < 8) {
+                                        return 'Password required!';
+                                      }
+                                    },
+                                    onSaved: (input) => _password = input,
+                                    decoration: InputDecoration(
+                                        icon: Icon(FontAwesomeIcons.key), hintText: 'Password'),
+                                    obscureText: true,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    RaisedButton(
+                      onPressed: signUp,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      color: Colors.red,
+                      child: new Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.add_box,
+                            color: Colors.white,
+                          ),
+                          new Container(
+                              padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                              child: new Text(
+                                "Register",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -95,9 +167,17 @@ class _RegisterState extends State<Register> {
             .user;
         user.sendEmailVerification();
 
-        await db.collection('users').document(user.uid).setData(
-            new UserModel(user.uid, user.email, 'newbie', 'user_role')
-                .toJson());
+        await db.collection('users').document(user.uid).setData(new UserModel(
+                user.uid,
+                user.email,
+                'newbie',
+                'user_role',
+                null,
+                user.email.substring(0, 3),
+                0,
+                0,
+                0)
+            .toJson());
 
         Navigator.pushReplacement(
             context,
