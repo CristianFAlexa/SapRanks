6d6c4df7fa5f7de10454f0b35b048f588685b865
I508587 <cristian.alexa@sap.com>
2019-10-31 11:56:33 +0200

    Added real time data from queue, refactored view.

--- lib/view/component/PlayGamePage.dart
+++ lib/view/component/PlayGamePage.dart
@@ -1,24 +1,28 @@
 import 'dart:async';
 
+import 'package:bored/service/DatabaseService.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 
 class PlayGamePage extends StatefulWidget {
-  PlayGamePage({this.user});
+  PlayGamePage({this.user, this.gameName});
 
   final FirebaseUser user;
+  final String gameName;
 
   @override
-  _PlayGamePageState createState() => _PlayGamePageState(user: user);
+  _PlayGamePageState createState() =>
+      _PlayGamePageState(user: user, gameName: gameName);
 }
 
 class _PlayGamePageState extends State<PlayGamePage> {
-  _PlayGamePageState({this.user});
+  _PlayGamePageState({this.user, this.gameName});
 
   final FirebaseUser user;
+  final String gameName;
 
   final duration = const Duration(seconds: 1);
   var swatch = Stopwatch();
@@ -48,8 +52,8 @@ class _PlayGamePageState extends State<PlayGamePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     startStopWatch();
+    super.initState();
   }
 
   @override
@@ -58,90 +62,145 @@ class _PlayGamePageState extends State<PlayGamePage> {
     swatch.stop();
   }
 
+  Widget getTextWidgets(List<dynamic> strings) {
+    return new Row(
+        children: strings
+            .map((item) => StreamBuilder(
+                  stream: collectionReference.document(item).snapshots(),
+                  builder: (context, snapshot) {
+                    return Padding(
+                      padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              SizedBox(
+                                width: 100,
+                              ),
+                              Container(
+                                width: 60,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: (snapshot
+                                                    .data['profile_picture'] ==
+                                                null)
+                                            ? AssetImage(
+                                                "assets/images/default-profile-picture.png")
+                                            : NetworkImage(snapshot
+                                                .data['profile_picture']))),
+                              ),
+                              Text(
+                                "${snapshot.data['name']}",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ))
+            .toList());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: GradientAppBar(
-        title: Center(
-          child: Text("Queue"),
-        ),
-        gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.black87, Colors.black38, Colors.black12]),
-        automaticallyImplyLeading: false,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-                Colors.blueGrey[800],
-                Colors.blueGrey[700],
-                Colors.blueGrey[700],
-                Colors.blueGrey[800],
-              ]),
-        ),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Text(
-                "Players: \n  2/10   ",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    backgroundColor: Colors.black12),
+    return StreamBuilder(
+        stream: queueCollectionReference.document(gameName).snapshots(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            appBar: GradientAppBar(
+              title: Center(
+                child: Text("Queue"),
               ),
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black87, Colors.black38, Colors.black12]),
+              automaticallyImplyLeading: false,
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Text(
-                "$timeDisplay",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    backgroundColor: Colors.black12),
+            body: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.blueGrey[800],
+                      Colors.blueGrey[700],
+                      Colors.blueGrey[700],
+                      Colors.blueGrey[800],
+                    ]),
               ),
-            ),
-            SizedBox(
-              height: 200,
-            ),
-            Center(
-              child: RaisedButton(
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                color: Colors.green,
-                child: new Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Icon(
-                      Icons.play_arrow,
-                      color: Colors.white,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Text(
+                      "Players: \n  ${snapshot.data['players'].length}/${snapshot.data['max_players']}",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          backgroundColor: Colors.black12),
                     ),
-                    new Container(
-                        padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                        child: new Text(
-                          "Start game",
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        )),
-                  ],
-                ),
+                  ),
+                  Center(
+                    child: getTextWidgets(snapshot.data['players']),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Text(
+                      "$timeDisplay",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          backgroundColor: Colors.black12),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 200,
+                  ),
+                  Center(
+                    child: RaisedButton(
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      color: Colors.green,
+                      child: new Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.play_arrow,
+                            color: Colors.white,
+                          ),
+                          new Container(
+                              padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                              child: new Text(
+                                "Start game",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
