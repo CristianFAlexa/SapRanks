5c9c5f35bf5a7210fdc7dfd2fa782ca4347c9634
I508587 <cristian.alexa@sap.com>
2019-11-28 16:05:07 +0200

    Widget for messages.

---
+++ lib/view/component/message/MessageNotification.dart
@@ -0,0 +1,50 @@
+import 'package:bored/model/Message.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class MessageNotification extends StatefulWidget {
+  @override
+  _MessageNotificationState createState() => _MessageNotificationState();
+}
+
+class _MessageNotificationState extends State<MessageNotification> {
+ final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+ final List<Message> messages = [];
+
+ @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        final notification = message['notification'];
+        setState(() {
+          messages.add(Message(
+            title: notification['title'], body: notification['body']
+          ));
+        });
+
+       // _showItemDialog(message);
+      },
+      //onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+       // _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+       // _navigateToItemDetail(message);
+      },
+    );
+  }
+
+  Widget buildMessage(Message message) => ListTile(
+      title: Text(message.title),
+      subtitle: Text(message.body),
+  );
+
+  @override
+  Widget build(BuildContext context) => ListView(
+    children: messages.map(buildMessage).toList(),
+  );
+}
